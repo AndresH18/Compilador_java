f6c72a3a9fe9bc2a245cce42da1bc703dfbfda2b
Andres <andres.hoyos18@eia.edu.co>
2021-08-26 20:44:49 -0500

    Implemented EnumSet

--- src/taller_2/ColumnType.java
+++ src/taller_2/ColumnType.java
@@ -1,19 +1,11 @@
 package taller_2;
 
 public enum ColumnType {
-    SYMBOL,
+    LEXEMA,
     LINE,
     COLUMN,
     TOKEN,
     TYPE,
     TYPE1,
     TYPE2;
-}
-
-enum SymbolsKeys {
-    NAME,
-    TYPE,
-    TYPE1,
-    TYPE2,
-    ;
 }
