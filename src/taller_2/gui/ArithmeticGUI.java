a77881bcfc2ed17be0a05068950f077b0b2ef15e
Andres <andres.hoyos18@eia.edu.co>
2021-08-27 19:38:35 -0500

    Arithmetic Expressions GUI, not yet implemented

---
+++ src/taller_2/gui/ArithmeticGUI.java
@@ -0,0 +1,13 @@
+package taller_2.gui;
+
+import javax.swing.*;
+
+public class ArithmeticGUI {
+    private JPanel mathPanel;
+    private JList list1;
+
+
+    public JPanel getMathPanel() {
+        return mathPanel;
+    }
+}
